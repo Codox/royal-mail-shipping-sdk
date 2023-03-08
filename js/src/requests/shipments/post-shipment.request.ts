01c878855d0750837f51e4f488aa86230c380d0c
Connor Brady <connorbrady@codox.eu>
2023-03-08 15:57:10 +0000

    ADDED POST Shipment request

--- js/src/requests/shipments/post-shipment.request.ts
+++ js/src/requests/shipments/post-shipment.request.ts
@@ -4,85 +4,95 @@ export interface POSTShipmentRequest {
         'X-RMG-Auth-Token': string;
     },
     body: {
-        'Shipper': {
-            'AddressId': string;
-            'ShipperReference': string;
-            'ShipperReference2': string;
-            'ShipperDepartment': string;
-            'CompanyName': string;
-            'ContactName': string;
-            'AddressLine1': string;
-            'AddressLine2': string;
-            'AddressLine3': string;
-            'Town': string;
-            'County': string;
-            'CountryCode': string;
-            'Postcode': string;
-            'PhoneNumber': string;
-            'EmailAddress': string;
-            'VatNumber': string;
+        Shipper: {
+            AddressId: string;
+            ShipperReference: string;
+            ShipperReference2: string;
+            ShipperDepartment: string;
+            CompanyName: string;
+            ContactName: string;
+            AddressLine1: string;
+            AddressLine2: string;
+            AddressLine3: string;
+            Town: string;
+            County: string;
+            CountryCode: string;
+            Postcode: string;
+            PhoneNumber: string;
+            EmailAddress: string;
+            VatNumber: string;
         },
-        'Destination': {
-            'AddressId': string;
-            'CompanyName': string;
-            'ContactName': string;
-            'AddressLine1': string;
-            'AddressLine2': string;
-            'AddressLine3': string;
-            'Town': string;
-            'County': string;
-            'CountryCode': string;
-            'Postcode': string;
-            'PhoneNumber': string;
-            'EmailAddress': string;
-            'VatNumber': string;
-        }
-        ,
-        'ShipmentInformation': {
-            'ShipmentDate': string;
-            'ServiceCode': string;
-            'ServiceOptions': {
-                'PostingLocation': string;
-                'ServiceLevel': string;
-                'ServiceFormat': string;
-                'Safeplace': string;
-                'SaturdayGuaranteed': boolean;
-                'ConsequentialLoss': string;
-                'LocalCollect': boolean;
-                'TrackingNotifications': string;
-                'RecordedSignedFor': boolean;
+        Destination: {
+            AddressId: string;
+            CompanyName: string;
+            ContactName: string;
+            AddressLine1: string;
+            AddressLine2: string;
+            AddressLine3: string;
+            Town: string;
+            County: string;
+            CountryCode: string;
+            Postcode: string;
+            PhoneNumber: string;
+            EmailAddress: string;
+            VatNumber: string;
+        },
+        ShipmentInformation: {
+            ShipmentDate: string;
+            ServiceCode: string;
+            ServiceOptions: {
+                PostingLocation: string;
+                ServiceLevel: string;
+                ServiceFormat: string;
+                Safeplace: string;
+                SaturdayGuaranteed: boolean;
+                ConsequentialLoss: string;
+                LocalCollect: boolean;
+                TrackingNotifications: string;
+                RecordedSignedFor: boolean;
             },
+            TotalPackages: number;
+            TotalWeight: number;
+            WeightUnitOfMeasure: string;
+            Product: string;
+            DescriptionOfGoods: string;
+            ReasonForExport: string;
+            Value: number;
+            Currency: string;
+            Incoterms: string;
+            LabelFormat: string;
+            SilentPrintProfile: string;
+            ShipmentAction: string;
+            Packages: {
+                PackageOccurrence: number;
+                PackagingId: string;
+                Weight: number;
+                Length: number;
+                Width: number;
+                Height: number;
+            }[],
+            Items: {
+                ItemId: string;
+                Quantity: number;
+                Description: string;
+                Value: number;
+                Weight: number;
+                PackageOccurrence: number;
+                HsCode: string;
+                SkuCode: string;
+                ImageUrl: string;
+            }[]
+        },
+        CustomsInformation: {
+            PreRegistrationNumber: string;
+            PreRegistrationType: string;
+            ShippingCharges: string;
+            OtherCharges: string;
+            QuotedLandedCost: string;
+            InvoiceNumber: string;
+            InvoiceDate: string;
+            ExportLicence: boolean;
+            AddresseeIdentificationReferenceNumber: string;
         }
-        'TotalPackages': number;
-        'TotalWeight': number;
-        'WeightUnitOfMeasure': string;
-        'Product': string;
-        'DescriptionOfGoods': string;
-        'ReasonForExport': string;
-        'Value': number;
-        'Currency': string;
-        'Incoterms': string;
-        'LabelFormat': string;
-        'SilentPrintProfile': string;
-        'ShipmentAction': string;
-        'Packages': {
-            'PackageOccurrence': number;
-            'PackagingId': string;
-            'Weight': number;
-            'Length': number;
-            'Width': number;
-            'Height': number;
-        }[],
-        'Items': {
-            'ItemId': string;
-            'Quantity': number;
-            'Description': string;
-            'Value': number;
-            'Weight': number;
-            'PackageOccurrence': number;
-            'HsCode': string;
-            'SkuCode': string;
-            'ImageUrl': string;
-        }[]
     }
 }
